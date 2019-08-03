049c9dc7d20399d4a97924b6c11df3e165bc027c
ISWB Prasetya <>
2019-08-03 11:49:59 +0200

    javadoc

--- src/main/java/nl/uu/cs/aplib/Agents/StateWithMessanger.java
+++ src/main/java/nl/uu/cs/aplib/Agents/StateWithMessanger.java
@@ -3,12 +3,22 @@ package nl.uu.cs.aplib.Agents;
 import nl.uu.cs.aplib.MainConcepts.SimpleState;
 import nl.uu.cs.aplib.MultiAgentSupport.Messenger;
 
+/**
+ * A version of {@link nl.uu.cs.aplib.MainConcepts.SimpleState} that also has a
+ * {@link nl.uu.cs.aplib.MultiAgentSupport.Messenger}. Through this Messenger,
+ * an agent that owns this state can send and receive messages to/from other
+ * agents.
+ * 
+ * @author Wish
+ *
+ */
 public class StateWithMessanger extends SimpleState {
 	
 	/**
-	 * A {@link Messenger} buffers incoming messages {@see Message} for the agent that
-	 * owns this state. It also provides messages to inspect and retrieve these messages,
-	 * as well as methods to send out messages to other agents.
+	 * A {@link nl.uu.cs.aplib.MultiAgentSupport.Messenger} buffers incoming
+	 * messages {@see Message} for the agent that owns this state. It also provides
+	 * messages to inspect and retrieve these messages, as well as methods to send
+	 * out messages to other agents.
 	 */
 	Messenger messenger = new Messenger() ;
 
