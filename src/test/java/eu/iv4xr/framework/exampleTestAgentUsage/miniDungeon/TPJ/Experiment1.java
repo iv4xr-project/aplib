f18eb176b704bd1863077adf0727443fd8bbe8d7
wooshrow <s.w.b.prasetya@uu.nl>
2025-03-03 11:08:23 +0100

    some edits on TPJ-exper1

--- src/test/java/eu/iv4xr/framework/exampleTestAgentUsage/miniDungeon/TPJ/Experiment1.java
+++ src/test/java/eu/iv4xr/framework/exampleTestAgentUsage/miniDungeon/TPJ/Experiment1.java
@@ -115,7 +115,7 @@ class Experiment1 {
 			runTest("shrineTest_singleplayer_" + config.configname + "_"  + smeagol.getId(),
 					smeagol,null, 
 					config,
-					new ShrineCleanTester().cleanseAllShrines(frodo, config.numberOfMaze),
+					new ShrineCleanTester().cleanseAllShrines(smeagol, config.numberOfMaze),
 					null,
 					shrineTestBudget,
 					true,true) ;		
